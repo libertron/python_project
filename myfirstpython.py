5ebfcaa28b1840b208e0ad4f45db7b4fc2ac5dfa
libertron <marcusserginio96@gmail.com>
2019-01-23 12:15:58 -0500

    solved the stars problem

--- myfirstpython.py
+++ myfirstpython.py
@@ -4,9 +4,32 @@
 # def myfunction(name):
 #     print("hello"+name)
 # myfunction("Stanj")
-def square(a,b):
-    return a*b
-a=input('first number ')
-b=input('second number ')
+# def square(a,b):
+#     return a*b
+# a=input('first number ')
+# b=input('second number ')
 # print("square of "+str(a)+" and "+str(b)+" is "+str(square(int(a),int(b))))
-print("square of {0} and {1} is {2} ".format(a,b,square(int(a),int(b))))
+# print("square of {0} and {1} is {2} ".format(a,b,square(int(a),int(b))))
+#fonction lambda (fonction qui retourne une seule ligne d'instruction)
+# calculer=lambda a,b:a+b
+# print(calculer(25,30))
+# import math 
+#or from (module_name) import (function_name or *   )
+# print(math.sqrt(50))
+# -------First--------
+# a=0
+# while a<=7:
+#     print(a*"*")
+#     a+=1
+# ----------Second
+# for i in range(1,8) :#si j'ai bien lu ça va de 1 à 7 (il faut rajouter 1 à 7, c'est bête mais bon...)
+#     print(i*'*')#on affiche i fois "*"
+
+# --------Third-----------
+# a=b=1
+# while a<=7:
+#     while b<a:
+#         print("*",end="")
+#         b+=1
+#     a+=1
+#     b=a
